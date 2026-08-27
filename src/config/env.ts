0ddb748c75b311172f8f45ee73fc8e69c7028173
khushiranucle5-b <khushiranucle5@gmail.com>
2026-08-27 11:59:11 +0530

    Refactor GOOGLE_MAPS_API_KEY declaration
    Remove default API key assignment for GOOGLE_MAPS_API_KEY.

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,5 +2,4 @@ declare const process: any;
 
 export const GOOGLE_MAPS_API_KEY: string =
   (typeof process !== 'undefined' && process?.env?.GOOGLE_MAPS_API_KEY) ||
-  (typeof process !== 'undefined' && process?.env?.REACT_APP_GOOGLE_MAPS_API_KEY) ||
-  'AIzaSyAl_rYWyYkPlDWbWiPuMg-SzrpPo11BiuY';
+  (typeof process !== 'undefined' && process?.env?.REACT_APP_GOOGLE_MAPS_API_KEY);
